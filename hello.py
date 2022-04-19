653bc9bb34f9e092cd3a846762fc802e9b91ce04
isidro <imiranda57@yahoo.com.ar>
2022-04-18 19:14:41 -0500

    1 commit

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hello, Word"
+    return "Hello, a Word  in peace"
 if __name__ == '__main__':
     app.run(port=5000, debug = True )

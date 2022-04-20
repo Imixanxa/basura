9ad0bfd9de82580b4b7f3719af72c94731801070
isidro <imiranda57@yahoo.com.ar>
2022-04-19 23:51:41 -0500

    primer cambio

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hello, a Word  in peace"
+    return "Hello, I want a Wordinpeace"
 if __name__ == '__main__':
     app.run(port=5000, debug = True )
 
 
+#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

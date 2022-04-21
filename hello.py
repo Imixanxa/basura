f0b9764d42d18235ef0cac634512fa1308dc880a
isidro <imiranda57@yahoo.com.ar>
2022-04-20 23:00:35 -0500

    first commit

--- hello.py
+++ hello.py
@@ -2,9 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "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 Hello, I want a Wordinpeace"
+    return "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 Hello, https://github.com/Imixanxa/firstrepository"
 if __name__ == '__main__':
     app.run(port=5000, debug = True )
 
 
 #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
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Imixanxa/basura.git
+git push -u origin main
+
+push an existing repository from the command line
+
+git remote add origin https://github.com/Imixanxa/basura.git
+git branch -M main
+git push -u origin main
+imixanxa
+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
+
+
+
+
